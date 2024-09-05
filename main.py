a26ceefb8ac0413816eb4c3bbaa6b69e0aa961d8
DanYS11 <dani.y.stevenson@gmail.com>
2024-09-05 13:05:17 +0200

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+import src.utils as util
+
+
+
+if __name__ == "__main__":
+    util.hello()
+    print("hello world")
---
+++ src/utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+import iisignature as isig
+
+def hello():
+    print("hello from utils")
+
+if __name__ == "__main__":
+    hello()
