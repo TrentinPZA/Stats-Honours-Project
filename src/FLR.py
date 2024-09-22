8cac6b3d74befc691f68a6114f41c8d5e04d5754
TrentinPZA <101876968+TrentinPZA@users.noreply.github.com>
2024-09-22 16:25:35 +0200

    Added BSpline FLR and fPRC

--- src/FLR.py
+++ src/FLR.py
@@ -8,67 +8,99 @@ from skfda.ml.regression import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 import matplotlib.pyplot as plt
+from skfda.preprocessing.dim_reduction.projection import FPCA
 from data_handler import import_air_qual
 
 X,Y = import_air_qual()
+def fitRegression(X,Y,type):
 
-#Splits the data into training/test data
-Xtrain, Xval, Ytrain, Yval = train_test_split(X, Y, test_size=0.33)
 
-dim_points = np.linspace(0,1,Xtrain.shape[1]) #Create a sequence of Xtrain.shape[1] points from 0 - 1.
-Xtrain_fdg = FDataGrid(Xtrain,dim_points)
-Xtest_fdg = FDataGrid(Xval,dim_points)
+    #Splits the data into training/test data
+    Xtrain, Xval, Ytrain, Yval = train_test_split(X, Y, test_size=0.33)
 
-#print(Xtrain_fdg)
-# Xtrain_fdg["dataset"].plot()
-# plt.show
+    dim_points = np.linspace(0,1,Xtrain.shape[1]) #Create a sequence of Xtrain.shape[1] points from 0 - 1.
+    Xtrain_fdg = FDataGrid(Xtrain,dim_points)
+    Xtest_fdg = FDataGrid(Xval,dim_points)
 
-basis_nums = [8,9,10,11,12,13,14,15,16]
-cv_reg_model = LinearRegression()
-basis_cv_mses = []
-for num_basis in basis_nums:
-    kfold_cv = KFold(n_splits=5)
-    current_mse = []
-    for train,test in kfold_cv.split(X):
-        basis_vectors = [Fourier(n_basis=num_basis) for _ in range(X.shape[2])] #Specifying that each dimension of the functional data will be expanded using a Fourier expansion
-        fourier_basis = VectorValued(basis_vectors) #Creating the Vector-Valued basis expansion object
+    #print(Xtrain_fdg)
+    # Xtrain_fdg["dataset"].plot()
+    # plt.show
+    if type == "Fourier" or type == "BSpline":
+        basis_nums = [8,9,10,11,12,13] #Fermanian did less than this but higher orders perform better.
+        cv_reg_model = LinearRegression()
+        basis_cv_mses = []
+        for num_basis in basis_nums:
+            kfold_cv = KFold(n_splits=5)
+            current_mse = []
+            for train_indices,test_indices in kfold_cv.split(X):
+                if type == "Fourier":
+                    basis_vectors = [Fourier(n_basis=num_basis) for _ in range(X.shape[2])] #Specifying that each dimension of the functional data will be expanded using a Fourier expansion
+                else:
+                    basis_vectors = [BSpline(n_basis=num_basis) for _ in range(X.shape[2])] #Specifying that each dimension of the functional data will be expanded using a Fourier expansion
 
-        Xtrain_CV,Ytrain_CV = X[train],Y[train]
-        Xtest_CV,Ytest_CV = X[test],Y[test]
+                fourier_basis = VectorValued(basis_vectors) #Creating the Vector-Valued basis expansion object
 
-        Xtrain_CV_fdg = FDataGrid(Xtrain_CV,dim_points)
-        Xtest_CV_fdg = FDataGrid(Xtest_CV,dim_points)
+                Xtrain_CV,Ytrain_CV = X[train_indices],Y[train_indices]
+                Xtest_CV,Ytest_CV = X[test_indices],Y[test_indices]
 
-        Xtrain_CV_fourier_basis = Xtrain_CV_fdg.to_basis(fourier_basis) 
-        Xtest_CV_fourier_basis = Xtest_CV_fdg.to_basis(fourier_basis)
-        
-        cv_reg_model.fit(Xtrain_CV_fourier_basis,Ytrain_CV)
-        cv_preds = cv_reg_model.predict(Xtest_CV_fourier_basis)
-        current_mse.append(np.mean((cv_preds-Ytest_CV) ** 2))
-    basis_cv_mses.append(np.mean(current_mse))
+                Xtrain_CV_fdg = FDataGrid(Xtrain_CV,dim_points)
+                Xtest_CV_fdg = FDataGrid(Xtest_CV,dim_points)
 
-optimal_basis_num=basis_nums[np.argmin(basis_cv_mses)]    
-print(optimal_basis_num)
+                Xtrain_CV_fourier_basis = Xtrain_CV_fdg.to_basis(fourier_basis) 
+                Xtest_CV_fourier_basis = Xtest_CV_fdg.to_basis(fourier_basis)
+                
+                cv_reg_model.fit(Xtrain_CV_fourier_basis,Ytrain_CV)
+                cv_preds = cv_reg_model.predict(Xtest_CV_fourier_basis)
+                current_mse.append(np.mean((cv_preds-Ytest_CV) ** 2))
 
-#Fitting a Linear SoFR using Fourier Basis Expansion with the optimal number of basis functions
+            basis_cv_mses.append(np.mean(current_mse))
 
-basis_vectors = [Fourier(n_basis=optimal_basis_num) for _ in range(X.shape[2])] #Specifying that each dimension of the functional data will be expanded using a Fourier expansion
-fourier_basis = VectorValued(basis_vectors)
-
-Xtrain_fourier_basis=Xtrain_fdg.to_basis(fourier_basis)
-Xtest_fourier_basis=Xtest_fdg.to_basis(fourier_basis)
-
-reg_model = LinearRegression()
-reg_model.fit(Xtrain_fourier_basis,Ytrain)
-
-#Getting predictions using the Linear SoFR model and calculate the MSE
-preds=reg_model.predict(Xtest_fourier_basis)
-print(np.mean((preds-Yval) ** 2))
+        optimal_basis_num=basis_nums[np.argmin(basis_cv_mses)]    
+        #print(optimal_basis_num)
+    else:
+        optimal_basis_num=7
 
 
 
 
 
+
+
+    #Fitting a Linear SoFR using Fourier Basis Expansion with the optimal number of basis functions
+    if type == "Fourier":
+        basis_vectors = [Fourier(n_basis=optimal_basis_num) for _ in range(X.shape[2])] #Specifying that each dimension of the functional data will be expanded using a Fourier expansion
+    elif type == "BSpline":
+        basis_vectors = [BSpline(n_basis=optimal_basis_num) for _ in range(X.shape[2])] #Specifying that each dimension of the functional data will be expanded using a BSpline expansion
+    else:
+        basis_vectors = [BSpline(n_basis=optimal_basis_num) for _ in range(X.shape[2])] #Specifying that each dimension of the functional data will be expanded using a BSpline expansion
+
+    fourier_basis = VectorValued(basis_vectors)
+
+    Xtrain_fourier_basis=Xtrain_fdg.to_basis(fourier_basis)
+    Xtest_fourier_basis=Xtest_fdg.to_basis(fourier_basis)
+    if type == "fPCA":
+        fpca_basis = FPCA(optimal_basis_num)
+        train_fpca_basis = fpca_basis.fit(Xtrain_fourier_basis)
+        test_fpca_basis = fpca_basis.fit(Xtest_fourier_basis)
+        Xtrain_fourier_basis = train_fpca_basis.transform(Xtrain_fourier_basis)
+        Xtest_fourier_basis = test_fpca_basis.transform(Xtest_fourier_basis)
+
+
+    reg_model = LinearRegression()
+    reg_model.fit(Xtrain_fourier_basis,Ytrain)
+
+    #Getting predictions using the Linear SoFR model and calculate the MSE
+    preds=reg_model.predict(Xtest_fourier_basis)
+    return(np.mean((preds-Yval) ** 2))
+
+
+FourierMSE = fitRegression(X,Y,"Fourier")
+BSplineMSE = fitRegression(X,Y,"BSpline")
+fPCRMSE = fitRegression(X,Y,"fPCA")
+
+print(FourierMSE)
+print(BSplineMSE)
+print(fPCRMSE)
 
 
 
