d8e711e4ac2d98dcd5a2d7d37c0baa6c1f9f6aa0
TrentinPZA <101876968+TrentinPZA@users.noreply.github.com>
2024-10-06 12:02:23 +0200

    Implemented code on new dataset

--- src/data_handler.py
+++ src/data_handler.py
@@ -5,7 +5,7 @@ import numpy as np
 def import_air_qual(multivariate=False):
     air_quality_data = pd.read_csv("data/AirQualityUCI.csv", sep=';', decimal=',', header=0) #Reading in the data, and specifying that the decimal points are commas so that they are instantly imported as floats.
     air_quality_data=air_quality_data.dropna(axis=0,how="all") #Dropping rows in the DF where all variables are NA
-
+    print(air_quality_data.shape[0])
     #Keeping only the variable of interest
     air_quality_data=air_quality_data[["PT08.S4(NO2)","T","RH","NO2(GT)"]] 
 
@@ -39,3 +39,35 @@ def import_air_qual(multivariate=False):
     #Fermanian scales this and I'm not sure why but it seems to be the right thing to do as it makes MSE small
     Y =Y/100
     return(X,Y)
+
+def import_energy_data(multivariate=False):
+    appliance_energy_data = pd.read_csv("data/energydata_complete.csv", sep=',',decimal=".",header=0) #Reading in the data, and specifying that the decimal points are commas so that they are instantly imported as floats.
+    half_length = len(appliance_energy_data) // 2
+
+    
+    appliance_energy_data = appliance_energy_data.iloc[:half_length]# Keep only the first half of the rows
+    appliance_energy_data=appliance_energy_data.dropna(axis=0,how="all")
+    
+    appliance_energy_data=appliance_energy_data[["Appliances","T1","T2","T3","T4","T5","T6","T7","T8","T9","T_out","Windspeed"]] 
+    
+    window = 180//10 #Using the past 3 hours to predict the next hour, must be integer division for For loop
+    x_unstacked=[]
+    y_unstacked=[]
+
+    # range(0,(len(appliance_energy_data)-window)) , This is how many windows of a specific size can be 'made' in a dataset of a specific length.
+    for i in range(0,(len(appliance_energy_data)-window)):
+        if multivariate==False:
+            x_window = appliance_energy_data['T_out'][i:window + i].to_numpy() 
+        else:
+            x_window = appliance_energy_data[["T1","T2","T3","T4","T5","T6","T7","Windspeed","T8","T9","T_out"]][i:window + i].to_numpy()  
+
+        x_unstacked.append(x_window) #Each Observation gets added to an array
+        y_unstacked.append(appliance_energy_data["Appliances"][window + i]) #Each target of the observation gets added to an array
+
+    #Puts the arrays into a 3D array (think of using cbind to create a dataframe in R)
+    #.shape gives d1 = number of observations,d2 = number of time points / data points in an observation, d3 = dimension
+    X = np.stack(x_unstacked)
+    Y = np.stack(y_unstacked)
+    Y =Y/100
+    return(X,Y)
+    
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,29 @@
 import numpy as np
-import iisignature as isig
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def hello():
-    print("hello from utils")
+def geom_interp():
+    appliance_energy_data = pd.read_csv("data/energydata_complete.csv", sep=',',decimal=".",header=0) #Reading in the data, and specifying that the decimal points are commas so that they are instantly imported as floats.  
+    appliance_energy_data = appliance_energy_data.iloc[10:26]# Keep only the first half of the rows
+    appliance_energy_data=appliance_energy_data[["T1","T2"]] 
+    
+    x = appliance_energy_data["T1"] 
+    y = appliance_energy_data["T2"] 
+    
+    fig, ax = plt.subplots()
+    ax.fill_between(x, y, min(y), color='lightblue', label=r'$S^{(j,i)}(X)$', alpha=0.7)
+    max_y = max(y)
+    ax.fill_between(x, y, max_y, where=(y <= max_y), color='orange', label=r'$S^{(i,j)}(X)$', alpha=0.5)
 
-if __name__ == "__main__":
-    hello()
+    ax.plot(x, y, marker='o', color='black')
+    ax.text(21.185, 19.9, r'$S^{(j)}(X)$', fontsize=10, color='red', verticalalignment='center', horizontalalignment='right')
+    ax.text(20.65, 19.4, r'$S^{(i)}(X)$', fontsize=10, color='red', verticalalignment='center', horizontalalignment='right')
+    ax.arrow(max(x),min(y), 0, max(y)-min(y)-0.1, head_width=0.05, head_length=0.1, fc='red', ec='red')
+    ax.arrow(min(x),min(y), max(x)-min(x)-0.1, 0, head_width=0.05, head_length=0.1, fc='red', ec='red')
+    ax.set_xlabel(r'$X^i$')
+    ax.set_ylabel(r'$X^j$')
+    ax.grid(True)
+    ax.set_xlim([min(x)-0.1, max(x)+0.1])
+    ax.set_ylim([min(y)-0.1,max(y)+0.1])
+    plt.legend()
+    plt.show()
