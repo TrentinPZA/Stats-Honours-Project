e01db1f2c7a5eb05739594e3d9cd5b12be03a762
TrentinPZA <101876968+TrentinPZA@users.noreply.github.com>
2024-10-09 08:45:28 +0200

    Various changes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
-from src import data_handler, RegressionModel,utils
 from data_handler import *
 from RegressionModel import *
 from utils import *
@@ -9,13 +8,28 @@ from sklearn.model_selection import train_test_split
 from skfda.ml.regression import LinearRegression
 import seaborn as sns
 import matplotlib.pyplot as plt
+import math
 
 def main():
     
+    if sys.argv[1]=="AirQuality":
+         
+         Kpen=0.1
+         if sys.argv[2]=="True":
+            X,Y = import_air_qual(multivariate=True)
+         else:
+            X,Y = import_air_qual(multivariate=False)
+         
+    elif sys.argv[1]=="ApplianceEnergy":
+        Kpen=0.5
+        if sys.argv[2]=="True":
+            X,Y = import_energy_data(multivariate=True)
+        else:
+            X,Y = import_energy_data(multivariate=False)
 
-    X,Y = import_air_qual(multivariate=True)
-    # geom_interp()
-       
+            
+
+    
     models = ["fPCA","BSpline","Fourier"]
     opt_nbasis=[]
     for model in models:
@@ -25,7 +39,7 @@ def main():
     
     for i, model_type in enumerate(models):
         for j in range(20):  
-            X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33)
+            X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33,random_state=j)
             model_ = BasisExpansionFLR(basis_exp_type=model_type, nbasis=opt_nbasis[i])
             model_.fit(X_train, Y_train)
             mse = model_.evaluate(X_test, Y_test)
@@ -33,20 +47,17 @@ def main():
 
     
     for k in range(20):
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33)
-        model = SignatureLinearModel()
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33,random_state=k+10)
+        TO_selector=SignatureOrderSelection(X_train.shape[2]+1,Kpen=Kpen)
+        trunc_order = TO_selector.get_hatm(X_train,Y_train, Kpen_values=np.e ** np.linspace(-3, 2, num=200))
+        print(trunc_order)
+        model = SignatureLinearModel(2)
         model.fit(X_train,Y_train)
         mses[3].append(model.evaluate(X_test,Y_test))
     
-    data = [mses[0], mses[1], mses[2], mses[3]]
-    labels = ['fPCR', 'BSpline', 'Fourier', "SLM"]
-    sns.boxplot(data=data)
-    plt.xticks([0, 1, 2, 3], labels)
-    plt.title('Comparison of MSEs of different models (FLR and SLM)')
-    plt.ylabel('MSE')
-    plt.show()
-    
-
+    MSEs = [mses[0], mses[1], mses[2], mses[3]]
+    plot_MSEs(MSEs=MSEs)
+   
     
     
 
