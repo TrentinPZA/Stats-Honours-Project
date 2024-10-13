61fadf2ab166859b6beac5912f040ecca968eedc
TrentinPZA <101876968+TrentinPZA@users.noreply.github.com>
2024-10-13 10:37:13 +0200

    Added extra parameter for more experiments

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ from skfda.ml.regression import LinearRegression
 import seaborn as sns
 import matplotlib.pyplot as plt
 import math
+from SignatureOrderCalculation import SignatureOrderSelection
 
 def main():
     
     if sys.argv[1]=="AirQuality":
          
-         Kpen=0.1
+         Kpen=0.01
          if sys.argv[2]=="True":
             X,Y = import_air_qual(multivariate=True)
          else:
@@ -33,6 +34,7 @@ def main():
     models = ["fPCA","BSpline","Fourier"]
     opt_nbasis=[]
     for model in models:
+        
         opt_nbasis.append(nbasis_cross_validation(basis_exp_type=model,X=X,Y=Y))
     
     mses = [[] for _ in range(4)] 
@@ -47,18 +49,25 @@ def main():
 
     
     for k in range(20):
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33,random_state=k+10)
-        TO_selector=SignatureOrderSelection(X_train.shape[2]+1,Kpen=Kpen)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33,random_state=k)
+
+        if sys.argv[1]=="ApplianceEnergy" and sys.argv[2]=="True":
+
+            if sys.argv[3]=="11":
+                TO_selector=SignatureOrderSelection(X_train.shape[2]+1,Kpen=Kpen,max_features=2*(10**3))
+            else:
+                TO_selector=SignatureOrderSelection(X_train.shape[2]+1,Kpen=Kpen)
+        else:
+                TO_selector=SignatureOrderSelection(X_train.shape[2]+1,Kpen=Kpen)
         trunc_order = TO_selector.get_hatm(X_train,Y_train, Kpen_values=np.e ** np.linspace(-3, 2, num=200))
-        print(trunc_order)
+        # print(trunc_order)
         model = SignatureLinearModel(trunc_order)
         model.fit(X_train,Y_train)
         mses[3].append(model.evaluate(X_test,Y_test))
     
     MSEs = [mses[0], mses[1], mses[2], mses[3]]
     plot_MSEs(MSEs=MSEs,d=X.shape[2])
-    
-    
+    # plot_coeff_heatmap(d=X.shape[2]+1,model=model)
     
 
 
